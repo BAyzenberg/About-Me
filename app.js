fc7f784921ab54381f9c15a6ef466a85e50cbc20
MichaelLeonTreat <michael.leon.treat@gmail.com>
2017-02-09 02:06:16 -0800

    Added a score feature that also prints out the score. By added a point to the score total for each corret answer

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var confirmQuiz = confirm('Hello, would you like to take a quiz?');
 
 // Creates and empty array called questionsAndAnswers which will store the questions and answers and eventually document.write them to the page.
 var questionsAndAnswers = [];
-
+var score =  0;
 // Checks if confirmQuiz is true or false and then console.logs
 if (confirmQuiz){
   console.log('Awesome, Lets get started!');
@@ -26,6 +26,8 @@ if(quizResponseOne.toUpperCase() === 'Y' || quizResponseOne.toUpperCase() === 'Y
   //Q&As getting pushed to the array. This will happend for each following quizResponseX if statement
   questionsAndAnswers.push(' Question: Does Mike own a business?');
   questionsAndAnswers.push(' Answer: Awesome! I own a window cleaning business call Apple Windows and Gutter!');
+  // added a score feature to keep track of how many the user got Correct
+  score += 1;
 } else {
   console.log('Sorry, that\'s incorrect! If you want your windows or gutters cleaned then give us a call!');
   //Q&As getting pushed to the array. This will happend for each following quizResponseX if statement
@@ -41,6 +43,8 @@ if (quizResponseTwo.toUpperCase() === 'Y' || quizResponseTwo.toUpperCase() === '
   console.log('Yeah I do. I\'ve got like 200 games on steam over the last 10 years!');
   questionsAndAnswers.push(' Question: Does Mike play PC games?');
   questionsAndAnswers.push(' Answer: Yeah I do. I\'ve got like 200 games on steam over the last 10 years!');
+  //score
+  score += 1;
 } else {
   console.log('Incorrect! I have a S*** ton of games on steam yet I\'m on Blizzard games like 90% of the time.');
   questionsAndAnswers.push(' Question: Does Mike play PC games?');
@@ -55,6 +59,8 @@ if (quizResponseThree.toUpperCase() === 'Y' || quizResponseThree.toUpperCase() =
   console.log('Yep! Once spring gets here my crew will use it for our business!');
   questionsAndAnswers.push(' Question: Does Mike have a truck?');
   questionsAndAnswers.push(' Question: Yep! Once spring gets here my crew will use it for our business!');
+  // score
+  score += 1;
 } else {
   console.log('Wrong. I have small work truck that my and my crew use for my business.');
   questionsAndAnswers.push(' Question: Does Mike own a business?');
@@ -68,6 +74,8 @@ if (quizResponseFour.toUpperCase() === 'Y' || quizResponseFour.toUpperCase() ===
   console.log('Yeah, but I\'m not very good. You can youTube a video of my getting my A$$ kicked in about 43 seconds hahah' );
   questionsAndAnswers.push(' Question: Does Mike Mike like MMA?');
   questionsAndAnswers.push(' Yeah, but I\'m not very good. You can youTube a video of my getting my A$$ kicked in about 43 seconds hahah');
+  //score
+  score += 1;
 } else {
   console.log('Nope, I actually do like MMA! I would like to get back into it again someday!');
   questionsAndAnswers.push(' Question: Does Mike like MMA?');
@@ -81,9 +89,12 @@ if (quizResponseFive.toUpperCase() === 'Y' || quizResponseFive.toUpperCase() ===
   console.log('Correct. More like I like Pad Thai food with my Peanut Sacue but whatever...');
   questionsAndAnswers.push(' Question: Does Mike like Thai food?');
   questionsAndAnswers.push(' Answer: Correct. More like I like Pad Thai food with my Peanut Sacue but whatever...');
+  //score
+  score += 1;
 } else {
   console.log('Wrong! I f!*$ing love Thai food!');
   questionsAndAnswers.push(' Question: Does Mike like Thai food?');
   questionsAndAnswers.push(' Answer: Wrong! I f!*$ing love Thai food!');
 }
-document.write(questionsAndAnswers);
+var totalScore = ' Score: ' + score + '!' ;
+document.write(questionsAndAnswers, totalScore);
